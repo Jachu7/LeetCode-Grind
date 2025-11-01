326ac5817aa3b08811faa521eef81062b66595c9
Jan Sularz <72702422+Jachu7@users.noreply.github.com>
2025-11-02 00:07:00 +0100

    Implement solution for largest number problem

---
+++ Medium/179-Largest Number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string largestNumber(vector<int> nums) {
+        vector<string> strNums;
+        for (int num : nums) {
+            strNums.push_back(to_string(num));
+        }
+
+        sort(strNums.begin(), strNums.end(), [](const string& s1, const string& s2) {
+            return (s1 + s2) > (s2 + s1);
+        });
+        
+        if (strNums[0] == "0") {
+            return "0";
+        }
+
+        string result = "";
+        for (const string& s : strNums) {
+            result += s;
+        }
+        
+        return result;
+    }
+};
