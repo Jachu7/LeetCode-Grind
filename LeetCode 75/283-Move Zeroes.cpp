87c70bc933e6249c71e6c46d228585dc768b66e4
Jan Sularz <72702422+Jachu7@users.noreply.github.com>
2026-01-15 11:16:28 +0100

    Implement moveZeroes function to shift zeros

---
+++ LeetCode 75/283-Move Zeroes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i = 0;
+        int count = 0;
+        while (i < nums.size() - count) {
+            if (nums[i] == 0) {
+                nums.erase(nums.begin() + i);
+                nums.push_back(0);
+                count++;
+            } 
+            else {
+                i++;
+            }
+        }
+    }
+};
